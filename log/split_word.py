9e0a03e63114a416b788221670fa1907ed894989
Haven Shi <haiwen.shi01@gmail.com>
2017-08-08 19:27:08 -0500

    snownlp

--- log/split_word.py
+++ log/split_word.py
@@ -10,7 +10,7 @@ s = SnowNLP(text)
 
 hashing = {}
 for item in s.words:  # s.words is array of split words
-    hashing[item] = hashing.get(item, 0) + 1  # record all words and frequencies into hashing map
+    hashing[item.lower()] = hashing.get(item.lower(), 0) + 1  # record all words and frequencies into hashing map
 
 # sort hashing
 key = sorted(hashing, key=hashing.get)[::-1]
@@ -23,4 +23,4 @@ newkey = np.array(key)
 newvalue = np.array(value)
 
 df = pd.DataFrame({"word" : newkey, "frequency" : newvalue})
-df.to_csv("split_word.csv", index=False)
+df.to_csv("split_word.csv", index=False, encoding="utf-8")
